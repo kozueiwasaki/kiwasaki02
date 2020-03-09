b819e0e938495d41300985714b3ba69ca214ba16
kocchan <59197715+kocchan@users.noreply.github.com>
2020-03-09 17:19:24 +0900

    changed files

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,6 +1,16 @@
 $(function(){
-  $(".toggleButton").click(function(){
-    console.log('hoge');
-    $(".globalNav").slideToggle();
+  // $(".toggleButton").click(function(){
+  //   console.log('hoge');
+  //   $(".globalNav").slideToggle();
+  // });
+  $('.toggleButton').on('click',function(){
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $('.globalNav').slideUp();
+      console.log('hoge');
+    } else {
+      $(this).addClass('active');
+      $('.globalNav').slideDown();
+    }
   });
 });
